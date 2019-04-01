c820b7c9ba86fb33b756d32f309b955205d02cd0
Chris J. Karr <chris@audacious-software.com>
2019-04-01 14:46:40 -0500

    Initial seed commit

---
+++ PassiveDataKit-iOS-HealthKit/PassiveDataKit_iOS_HealthKit.h
@@ -0,0 +1,19 @@
+//
+//  PassiveDataKit_iOS_HealthKit.h
+//  PassiveDataKit-iOS-HealthKit
+//
+//  Created by Chris Karr on 4/1/19.
+//  Copyright © 2019 Audacious Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PassiveDataKit_iOS_HealthKit.
+FOUNDATION_EXPORT double PassiveDataKit_iOS_HealthKitVersionNumber;
+
+//! Project version string for PassiveDataKit_iOS_HealthKit.
+FOUNDATION_EXPORT const unsigned char PassiveDataKit_iOS_HealthKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PassiveDataKit_iOS_HealthKit/PublicHeader.h>
+
+
